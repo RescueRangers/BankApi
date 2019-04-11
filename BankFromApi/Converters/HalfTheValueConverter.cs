c62813a27d9a039fbf1392a3091cb59f7e302943
Radosław Radomski <pryq11@gmail.com>
2019-04-11 11:39:42 +0200

    Made the chart listview display 4 charts no matter the window size.

---
+++ BankFromApi/Converters/HalfTheValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BankFromApi.Converters
+{
+    public class HalfTheValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var actualValue = (double)value;
+
+                return (actualValue - 30) / 2;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
